ef0caa957946052ad8d25fe4e048e174524b77e9
Ande <andeschoonover@gmail.com>
2021-01-27 14:03:31 -0800

    Setup Random Backgrounds Color changes Utilizes array, array.length, and CSS Classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Okay!");
+
+const backgrounds = ["bg0", "bg1", "bg2"];
+
+let numRand;
+
+numRand = Math.floor(Math.random() * backgrounds.length);
+
+let pageBackground = document.querySelector("body");
+
+pageBackground.className = backgrounds[numRand];
