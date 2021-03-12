7807627ece54119cce8bc081ea6c3986a004d494
Ande <andeschoonover@gmail.com>
2021-03-12 15:17:12 -0800

    LightGallery and Start of Styling FINALLY WORKING. THANK GOODNESS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-console.log("Okay!");
+/*console.log("Okay!");
 
 const backgrounds = ["bg0", "bg1", "bg2"];
 
@@ -9,3 +9,9 @@ numRand = Math.floor(Math.random() * backgrounds.length);
 let pageBackground = document.querySelector("body");
 
 pageBackground.className = backgrounds[numRand];
+*/
+const gallery = document.getElementById("gallery");
+
+lightGallery(gallery, {
+  selector: "a"
+});
